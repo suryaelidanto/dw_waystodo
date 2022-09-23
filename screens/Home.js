98c5eb0413dd9ef98a1dc445adbb440f9a6204d5
suryaa6666 <suryaelidanto@gmail.com>
2022-09-23 22:51:26 +0700

    UPDATE : home list, category, with dynamic bg color

--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,78 @@
-import { Box, Text, Menu, Button, Image } from "native-base";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Box, Button, Image, Menu, Text } from "native-base";
 import { useContext, useState } from "react";
-import { UserContext } from "../context/userContext";
 import { showMessage } from "react-native-flash-message";
 import { useQuery } from "react-query";
 import { API } from "../config/api";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { UserContext } from "../context/userContext";
 
 function Home({ navigation }) {
   const [state, dispatch] = useContext(UserContext);
   const [shouldOverlapWithTrigger] = useState(false);
+  const todoColor = [
+    {
+      index: 0,
+      bgColor: "primary.200",
+    },
+    {
+      index: 1,
+      bgColor: "green.200",
+    },
+    {
+      index: 2,
+      bgColor: "danger.200",
+    },
+    {
+      index: 3,
+      bgColor: "warning.200",
+    },
+    {
+      index: 4,
+      bgColor: "purple.200",
+    },
+  ];
+
+  const categoryColor = [
+    {
+      index: 0,
+      bgColor: "danger.500",
+    },
+    {
+      index: 1,
+      bgColor: "purple.500",
+    },
+    {
+      index: 2,
+      bgColor: "green.500",
+    },
+    {
+      index: 3,
+      bgColor: "primary.500",
+    },
+    {
+      index: 4,
+      bgColor: "warning.500",
+    },
+  ];
 
   let { data: list } = useQuery("listCaches", async () => {
-    const response = await API.get("/List");
-    console.log("response list", response.data);
-    return response.data;
+    let listResponse = await API.get("/List");
+    console.log("response list", listResponse.data);
+    return listResponse.data;
   });
+
+  let { data: category } = useQuery("categoryCaches", async () => {
+    let categoryResponse = await API.get("/Category");
+    console.log("category list", categoryResponse.data);
+    return categoryResponse.data;
+  });
+
+  // intinya buat cut kalimat, kalau dia lebih dari max character, ya potong terus tambahin ...
+  function cutSentence(sentence, maxCharacter) {
+    return sentence.length > maxCharacter
+      ? sentence.substring(0, maxCharacter) + "..."
+      : sentence;
+  }
 
   function handleLogout() {
     AsyncStorage.removeItem("token");
@@ -65,6 +123,82 @@ function Home({ navigation }) {
             <Menu.Item onPress={handleLogout}>Logout</Menu.Item>
           </Menu>
         </Box>
+      </Box>
+
+      {/* list todo */}
+      <Box w={"90%"}>
+        {list?.map((item, i) => {
+          return (
+            <Box
+              // intinya dia ngambil dari data todo color kita find ngecompare index sama index item terus di modulus banyaknya todoColor
+              // object keys itu ngambil banyaknya length terus di - 1 biar modulus 4, jadi loop balik ke index awal lagi
+              bg={
+                todoColor?.find(
+                  (item) =>
+                    item.index === i % (Object.keys(todoColor).length - 1)
+                ).bgColor
+              }
+              w={"100%"}
+              borderRadius={10}
+              display="flex"
+              flexDirection="row"
+              px={5}
+              py={5}
+              key={i}
+              my={2}
+            >
+              <Box flex={2}>
+                <Text fontWeight="bold" fontSize={20}>
+                  {cutSentence(item.name, 15)}
+                </Text>
+                <Text color="muted.500">
+                  {cutSentence(item.description, 20)}
+                </Text>
+                <Text color="muted.500">{item.date}</Text>
+              </Box>
+              <Box flex={1}>
+                <Box
+                  p={1}
+                  borderRadius={10}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  // intinya dia ngambil dari data todo color kita find ngecompare index sama index item terus di modulus banyaknya todoColor
+                  // object keys itu ngambil banyaknya length terus di - 1 biar modulus 4, jadi loop balik ke index awal lagi
+                  bg={
+                    categoryColor?.find(
+                      (item) =>
+                        item.index ===
+                        i % (Object.keys(categoryColor).length - 1)
+                    ).bgColor
+                  }
+                >
+                  <Text color="white" fontWeight="bold">
+                    {
+                      category?.find(
+                        (itemCategory) => itemCategory._id === item.category_id
+                      ).name
+                    }
+                  </Text>
+                </Box>
+                <Box
+                  flex={1}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Button
+                    bg="muted.300"
+                    borderRadius={"100%"}
+                    mt={2}
+                    w={50}
+                    h={50}
+                  ></Button>
+                </Box>
+              </Box>
+            </Box>
+          );
+        })}
       </Box>
     </Box>
   );
